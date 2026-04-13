8ec082ff4e7abfa11464a5fff29abc0c0194df15
Anand-1812 <anand.work2005@gmail.com>
2026-04-14 02:26:20 +0530

    how to read file from cmd line

--- basics/dup.go
+++ basics/dup.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-  "bufio"
-  "fmt"
-  "os"
+	"bufio"
+	"fmt"
+	"os"
 )
 
 func main() {
+	// text of each lines appears more than ones
   counts := make(map[string]int)
   input := bufio.NewScanner(os.Stdin)
 
@@ -19,4 +20,35 @@ func main() {
       fmt.Printf("%d\t%s\n", n, line)
     }
   }
+
+	// text of lines appear more than once
+	counts1 := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts1)
+	} else {
+		for _, args := range files {
+			f, err := os.Open(args)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+
+			countLines(f, counts1)
+			f.Close()
+		}
+
+		for line, n := range counts1 {
+			if n > 1 {
+				fmt.Printf("%d\t%s\n", n, line)
+			}
+		}
+	}
+}
+
+func countLines(f *os.File, count map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		count[input.Text()]++
+	}
 }
