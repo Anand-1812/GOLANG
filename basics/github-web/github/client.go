627263d68b99e8a2475b8a0fb46219e1c897015c
Anand-1812 <anand.work2005@gmail.com>
2026-04-30 23:19:56 +0530

    fix: the url of fetching and typo issue to issues

--- basics/github-web/github/client.go
+++ basics/github-web/github/client.go
@@ -2,18 +2,23 @@ package github
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"net/url"
 )
 
-const IssueURL = "https://api.github.com/search/issue?q="
+const IssueURL = "https://api.github.com/search/issues?q="
 
 func FetchIssue(query string) (*IssuesSearchResult, error) {
-	res, err := http.Get(IssueURL + query)
+	res, err := http.Get(IssueURL + url.QueryEscape(query))
 	if err != nil {
 		return nil, err
 	}
 
 	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("github search failed: %s", res.Status)
+	}
 
 	var result IssuesSearchResult
 	if err := json.NewDecoder(res.Body).Decode(&result); err != nil {
--- basics/github-web/main.go
+++ basics/github-web/main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github-web/github"
 	"github-web/web"
 )
 
 func main() {
-	err := github.LoadData("repo:golang/go+is:issue+bug")
+	err := github.LoadData("repo:golang/go is:issue bug")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -19,6 +20,11 @@ func main() {
 	http.HandleFunc("/users", web.UsersHandler)
 	http.HandleFunc("/milestones", web.MilestonesHandler)
 
-	log.Println("Server running at http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Println("Server running at http://localhost:" + port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
